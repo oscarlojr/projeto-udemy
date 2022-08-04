6dfac479cc7c108e86c1996755328e3811b0897d
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2022-08-03 21:25:12 -0300

    Testes com o novo componente

--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./title.component.scss']
 })
 export class TitleComponent implements OnInit {
-  public title: string = "Teste";
+  public title: string = "Teste!";
   constructor() { }
 
   ngOnInit(): void {
