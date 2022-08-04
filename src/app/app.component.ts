d1e0bc4d6054704d68d253a372a25c1bc4d162ae
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2022-08-04 07:29:34 -0300

    Aplicando conceitos de ngOnChanges

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<router-outlet></router-outlet>',
+  template: 
+  `
+    <app-title title="Olá Mundo"></app-title>
+    <router-outlet></router-outlet>
+  `,
 })
 export class AppComponent implements OnInit{
   constructor() {}
--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.scss']
 })
-export class TitleComponent implements OnInit {
-  public title: string = "Teste!";
+export class TitleComponent implements OnInit, OnChanges {
+  @Input() public title: string = "Teste!";
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngOnChanges(): void {
+    console.log("Foi alterado com sucesso")
+  }    
 }
