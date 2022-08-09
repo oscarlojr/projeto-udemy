cbf2087c349a37785b2487cb4f5b31c5a119b73e
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2022-08-08 21:55:59 -0300

    Aplicando conceitos de property data-binding

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -6,8 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./data-binding.component.scss']
 })
 export class DataBindingComponent implements OnInit {
+  
   public nome: string = "Oscar";
   public idade: number = 37;
+  public checkedDisabled: boolean = true;
+  public imgSrc: string = "https://pt.apkshki.com/storage/8970/icon_60edf3fb44c6f_8970_w256.png";
+  public descricao: string = "Goku Dragon ball";
+
   constructor() { }
 
   ngOnInit(): void {
