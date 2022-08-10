5b5d05fcd82272bb5b1f6babf8311d5664fdb15c
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2022-08-10 20:48:48 -0300

    Aplicando conceitos de ngFor

--- src/app/diretivas-estruturais/diretivas-estruturais.component.ts
+++ src/app/diretivas-estruturais/diretivas-estruturais.component.ts
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./diretivas-estruturais.component.scss']
 })
 export class DiretivasEstruturaisComponent implements OnInit {
+  public list: Array<{ nome: string, idade: number}> = [
+    {nome: "Oscar", idade: 37},
+    {nome: "Marcelo", idade: 23},
+    {nome: "Dener", idade: 46},
+  ];
   public condition: boolean = true;
   public conditionClick: boolean = true;
   constructor() { }
@@ -26,6 +31,14 @@ export class DiretivasEstruturaisComponent implements OnInit {
     }else{
       this.conditionClick = true;
     }
+  }
+
+  public clickOnAddList(){
+    this.list.push({nome: "Nay", idade: 27})
+  }
+
+  public onClickEventList(event: number){
+    this.list.splice(event,1);
   }
 
 }
