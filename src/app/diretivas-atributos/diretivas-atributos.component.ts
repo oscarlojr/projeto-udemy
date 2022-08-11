fafa2d718db914b867b337481a0cfbb4fd15f1da
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2022-08-11 19:21:33 -0300

    Aplicando conceitos de ngModel

--- src/app/diretivas-atributos/diretivas-atributos.component.ts
+++ src/app/diretivas-atributos/diretivas-atributos.component.ts
@@ -7,16 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiretivasAtributosComponent implements OnInit {
   public valor: boolean = true;
+  public heightPx: string = "20px";
+  public background: string = "red";
+  public nome: string = "";
+
+  public salvar() {
+    this.list.push({nome: this.nome});
+    this.nome = "";
+  }
+  public limpar() {
+    
+  }
   constructor() { }
+
+  public list: Array<{ nome: string }> = [];
 
   ngOnInit(): void {
     setInterval (() => {
       if(this.valor){
-        this.valor = false;
+        this.valor = false;       
       }else {
-        this.valor = true;
+        this.valor = true;        
       }
-    },2000)
-  }
 
+      if( this.heightPx == "20px"){
+        this.heightPx = "50px";
+        this.background = "green";
+      }else {
+        this.heightPx = "20px";
+        this.background = "yellow";
+      }
+    },2000);   
+  } 
 }
