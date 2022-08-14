f3d8c6e72bbf25c79e45e3234904e03dcf6fe004
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2022-08-14 20:46:58 -0300

    Aplicando conceitos de ng-content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,10 @@ import { Component, OnInit } from '@angular/core';
     <!--
       <app-diretivas-estruturais></app-diretivas-estruturais>
     -->
-    <app-diretivas-atributos></app-diretivas-atributos>
+    <app-diretivas-atributos>
+      <h1>Aulas de diretivas de atributo</h1>
+      <h3>Fim do bloco</h3>
+    </app-diretivas-atributos>
     <router-outlet></router-outlet>
   `,
 })
