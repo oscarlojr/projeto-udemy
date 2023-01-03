8d7c85b10b950598c87a6821092a0522bac87226
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2023-01-03 07:36:38 -0300

    Aplicando conceitos de redirecionamento de rotas com TS

--- src/app/pages/sobre/sobre.component.ts
+++ src/app/pages/sobre/sobre.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute,  Router } from '@angular/router';
 
 @Component({
   selector: 'app-sobre',
@@ -8,7 +8,10 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class SobreComponent implements OnInit {
 
-  constructor(private activatedRoute:ActivatedRoute) { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(
@@ -17,6 +20,10 @@ export class SobreComponent implements OnInit {
     this.activatedRoute.queryParams.subscribe(
       res => console.log(res)
     )
+    setInterval(() => {
+      this.router.navigate(['404']);
+      //this.router.navigateByUrl('404')
+    }, 5000)
   }
 
 }
